d1ccfb7390592536286b531d10249fdb7407c2b0
notflip <miguel@notflip.be>
2018-02-14 23:20:55 +0100

    Notifier Complete change, less focus on shots

--- src/views/Notifier.vue
+++ src/views/Notifier.vue
@@ -2,16 +2,13 @@
     <Modal v-model="modal.show">
         <template slot="content">
 
-            <h1 class="ui icon header" :class="modal.color">
-                <i class="massive trophy icon"></i>
+            <h1 class="ui header inverted">
+                <i class="alarm icon"></i>
                 <div class="content">
-                    <span v-html="modal.text"></span><br>
+                    <div class="text" v-html="modal.text"></div>
+                    <div class="subtext" v-html="modal.subtext"></div>
                 </div>
             </h1>
-
-            <h2 class="ui icon header" :class="modal.color">
-                <span class="subtext" v-html="modal.subtext"></span>
-            </h2>
 
         </template>
     </Modal>
@@ -23,10 +20,12 @@
     import Modal from '../components/Modal.vue';
 
     const modalDuration = 15 * 1000; // 15 seconds
-    const modalTimeout = 30 * 1000 * 60; // 30 minutes
+    const modalTimeout = 1 * 1000 * 60; // 30 minutes
     const modalMultiplier = 0.1;
 
-    const texts = [
+    const minimumShots = 4;
+
+    const quotes = [
         '&quot;Alcohol may be man\'s worst enemy, but the bible says love your enemy&quot;'
     ];
 
@@ -38,10 +37,6 @@
             return {
                 users: [],
                 shots: [],
-                previous: {
-                    total: 0,
-                    highest: 0
-                },
                 modal: {
                     show: false,
                     text: '',
@@ -58,32 +53,39 @@
 
             this.$watch;
 
-            // Periodic Checks
-            setInterval(this.checkBestSupporter, modalTimeout * modalMultiplier);
-
-            // Periodic Check with Offset
-            let offset = setInterval(() => {
-                clearInterval(offset);
-                setInterval(this.checkTimeSinceLastShot, modalTimeout * modalMultiplier);
-            }, (modalTimeout / 2) * modalMultiplier);
-
+            // Intervals
+            setInterval(() => {
+                this.checkBestSupporter();
+            }, modalTimeout * modalMultiplier);
 
         },
         watch: {
             shots(shots) {
 
-                let highestShot = shots.reduce((max, shot) => {
-                    return shot.amount > max.amount ? shot : max;
-                }, {amount: 0});
+                if (!this.modal.show && shots) {
 
-                if (!this.modal.show) {
-                    if (highestShot.amount !== this.previous.highest && highestShot.amount > 1) {
+                    let newestShot = shots.reduce((a, b) => a.timestamp > b.timestamp ? a : b, 0);
+
+                    if (newestShot.amount > minimumShots) {
                         this.modal.show = true;
-                        this.modal.text = 'Nieuw Record!<br>' + highestShot.amount + ' shots besteld door <br><span class="capitalize">' + this.findUserById(highestShot.userid).nickname + '</span>';
-                        this.modal.color = 'yellow';
+                        this.modal.text = newestShot.amount + ' shots besteld door <br><span class="capitalize">' + this.findUserById(newestShot.userid).nickname + '</span>';
+                        this.modal.subtext = quotes[Math.floor(Math.random() * quotes.length)];
                     }
 
-                    this.previous.highest = Number(highestShot.amount);
+                    setTimeout(() => {
+                        this.modal.show = false;
+                    }, modalDuration * modalMultiplier)
+                }
+            },
+            users(users) {
+
+                if (!this.modal.show) {
+
+                    let newestUser = users.reduce((a, b) => a.identifier > b.identifier ? a : b, 0);
+
+                    this.modal.show = true;
+                    this.modal.text = 'Bedankt ' + newestUser.name + ' om ons avontuur te steunen';
+                    this.modal.subtext = quotes[Math.floor(Math.random() * quotes.length)];
 
                     setTimeout(() => {
                         this.modal.show = false;
@@ -91,47 +93,7 @@
                 }
             }
         },
-        users(users) {
-
-            let totalShots = users.reduce((total, user) => {
-                return Number(total) + Number(user.shots);
-            }, 0);
-
-            if (!this.modal.show) {
-                if (totalShots !== this.previous.total && totalShots % 5 === 0 && totalShots >= 5) {
-                    this.modal.show = true;
-                    this.modal.text = totalShots + ' shots gepasseerd. Keep it up';
-                    this.modal.subtext = texts[Math.floor(Math.random() * texts.length)];
-                    this.modal.color = 'inverted';
-                }
-
-                this.previous.total = Number(totalShots);
-
-                setTimeout(() => {
-                    this.modal.show = false;
-                }, modalDuration * modalMultiplier)
-            }
-        },
         methods: {
-            checkTimeSinceLastShot() {
-
-                if (!this.modal.show) {
-
-                    let lastShot = this.shots.reduce((max, current) => current.timestamp > max.timestamp ? current : max, {timestamp: 0});
-
-                    if(lastShot.timestamp) {
-                        this.modal.show = true;
-                        this.modal.text = 'De laatste shot is al ' + this.$moment().to(lastShot.timestamp);
-                        this.modal.subtext = 'De bar verveelt zich te pletter';
-                        this.modal.color = 'inverted';
-
-                        setTimeout(() => {
-                            this.modal.show = false;
-                        }, modalDuration * modalMultiplier)
-                    }
-
-                }
-            },
             checkBestSupporter() {
 
                 if (!this.modal.show) {
@@ -140,12 +102,10 @@
                         .filter(user => user.credits > 0)
                         .reduce((a, b) => a.credits > b.credits ? a : b);
 
-                    if(bestSupporter) {
+                    if (bestSupporter) {
                         this.modal.show = true;
                         this.modal.text = 'Onze beste supporter is momenteel ' + bestSupporter.name;
                         this.modal.subtext = '';
-
-                        this.modal.color = 'inverted';
 
                         setTimeout(() => {
                             this.modal.show = false;
